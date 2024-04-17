cf56e743c55e0f01ac4f74b72a04a96eef09ce61
dmtrth25 <vodolazhchenko25@gmail.com>
2024-04-17 13:43:20 +0300

    refactored client issues for sprint 6

--- src/pages/tutor-home/TutorHome.tsx
+++ src/pages/tutor-home/TutorHome.tsx
@@ -1,19 +1,16 @@
 import { useEffect } from 'react'
-import { useTranslation } from 'react-i18next'
 
 import { useAppSelector } from '~/hooks/use-redux'
 import { useModalContext } from '~/context/modal-context'
 
 import UserStepsWrapper from '~/components/user-steps-wrapper/UserStepsWrapper'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
-import PopularCategories from '~/components/popular-categories/PopularCategories'
 import FindBlock from '~/components/find-block/FindBlock'
 
 import { styles } from '~/pages/tutor-home/TutorHome.styles'
 import { translationKey } from '~/components/find-block/find-student-constants'
 
 const TutorHome = () => {
-  const { t } = useTranslation()
   const { openModal } = useModalContext()
   const { isFirstLogin, userRole } = useAppSelector((state) => state.appMain)
 
@@ -31,10 +28,6 @@ const TutorHome = () => {
   return (
     <PageWrapper data-testid='tutorHome'>
       <FindBlock translationKey={translationKey} />
-      <PopularCategories
-        description={t('tutorHomePage.popularCategories.description')}
-        title={t('tutorHomePage.popularCategories.title')}
-      />
     </PageWrapper>
   )
 }
