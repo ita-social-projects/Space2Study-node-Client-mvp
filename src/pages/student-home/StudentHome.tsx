c5a971c4a7d0e70aadba100b7bf532049079d977
Dmytro <56305508+dmtrth25@users.noreply.github.com>
2024-04-23 18:37:39 +0300

    refactored client issues for sprint 6 (#94)

--- src/pages/student-home/StudentHome.tsx
+++ src/pages/student-home/StudentHome.tsx
@@ -1,20 +1,15 @@
 import { useEffect } from 'react'
-import { useTranslation } from 'react-i18next'
 import Container from '@mui/material/Container'
 
 import { useAppSelector } from '~/hooks/use-redux'
 import { useModalContext } from '~/context/modal-context'
 import UserStepsWrapper from '~/components/user-steps-wrapper/UserStepsWrapper'
 import FindBlock from '~/components/find-block/FindBlock'
-import PopularCategories from '~/components/popular-categories/PopularCategories'
 import Faq from '~/containers/student-home-page/faq/Faq'
-import StudentHowItWorks from '~/containers/student-home-page/student-how-it-works/StudentHowItWorks'
 
 import { translationKey } from '~/components/find-block/find-tutor-constants'
 
 const StudentHome = () => {
-  const { t } = useTranslation()
-
   const { openModal } = useModalContext()
   const { isFirstLogin, userRole } = useAppSelector((state) => state.appMain)
 
@@ -37,11 +32,7 @@ const StudentHome = () => {
   return (
     <Container data-testid='studentHome' sx={{ flex: 1 }}>
       <FindBlock translationKey={translationKey} />
-      <PopularCategories
-        description={t('studentHomePage.popularCategories.description')}
-        title={t('studentHomePage.popularCategories.title')}
-      />
-      <StudentHowItWorks />
+      {/*implement here*/}
       <Faq />
     </Container>
   )
